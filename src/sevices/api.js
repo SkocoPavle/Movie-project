11a10dd30417e17537a8eadb86a02d5cacfa3e7e
Skoco Pavle <skocopavle@gmail.com>
2025-06-12 19:25:25 +0200

    Here l made an api.js application for base_url and api_key

--- src/sevices/api.js
+++ src/sevices/api.js
@@ -3,4 +3,12 @@ const BASE_URL = "https://www.themoviedb.org/";
 
 export const getPopularMovies = async () => {
     const response = await fetch(`${BASE_URL}/movies/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
 };
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movies?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+}
